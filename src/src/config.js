d85ed1a18016fe69ed5cca5e0431287e44ea31fa
pinghuang <ping.huang@icfi.com>
2020-03-17 15:17:06 -0400

    Update config.js

--- src/src/config.js
+++ src/src/config.js
@@ -4,9 +4,17 @@ const OCL_DOMAIN = (window.OCL_DOMAIN) || 'staging.openconceptlab.org';
 
 const config = { 
   "domain": OCL_DOMAIN, 
-  "org": "PEPFAR-Test6", 
-  "source": "-Test6",
-  "defaultYear": "2020" }
+  "org": "PEPFAR-Test7", 
+  "source": "",
+  "defaultYear": "2020" ,
+  "versionMap": [
+    {"year": "2020","version": "v2.4","fromToText": "from MER 2.3 to 2.4"},
+    {"year": "2019","version": "v2.3","fromToText": "from MER 2.2 to 2.3"},
+    {"year": "2018","version": "v2.2","fromToText": "from MER 2.1 to 2.2"},
+    {"year": "2017","version": "v2.1","fromToText": "from MER 2.0 to 2.1"},
+    {"year": "2016","version": "v2.0","fromToText": "from MER 1.0 to 2.0"}
+  ]
+}
 
 
 
