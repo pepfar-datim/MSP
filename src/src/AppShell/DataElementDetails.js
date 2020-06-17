fabe31be977d2cc8663861c672db35821b74435b
Hiwot <hiwot-chichaybelu@github.com>
2020-06-17 14:13:57 -0400

    Fixed expandible tree view

--- src/src/AppShell/DataElementDetails.js
+++ src/src/AppShell/DataElementDetails.js
@@ -852,7 +852,48 @@ export default function DataElementDetails() {
             //   `Error when retrieving data element  ${e.message}`
             // );
         }
+        
     }
+      //Error handling
+  class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { error: null, errorInfo: null };
+    }
+
+    componentDidCatch(error, errorInfo) {
+      // Catch errors in any components below and re-render with error message
+      this.setState({
+        error: error,
+        errorInfo: errorInfo
+      })
+      // You can also log error messages to an error reporting service here
+    }
+
+    render() {
+      if (this.state.errorInfo) {
+        // Error path
+        return (
+          <Dialog onClose={handleDialogClose} aria-labelledby="customized-dialog-title" open={dialogOpen}>
+            <DialogTitle id="customized-dialog-title" onClose={handleDialogClose}>
+            </DialogTitle>
+            <DialogContent >
+              <Typography gutterBottom>
+                {dialogMessage}
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button autoFocus onClick={handleDialogClose} color="primary">
+                OK
+          </Button>
+            </DialogActions>
+          </Dialog>
+        );
+      }
+      // Normally, just render children
+      return this.props.children;
+    }
+  }
 
     return (
         <Route render={(history) => (
@@ -1194,6 +1235,7 @@ export default function DataElementDetails() {
                                             </div> */}
                                 {/* {showLinked ? <div style={{ padding: '20px', marginLeft: '170px' }}>or select a linked data element below</div> : ''} */}
                                 <div style={{ margin: '15px' }}>
+                                <ErrorBoundary>
                                     <Tabs value={panel} onChange={handleChange} className={classes.tabContainer} classes={{ indicator: classes.bigIndicator }}>
                                         <Tab label="DISAGGREGATIONS" {...a11yProps(0)} />
                                         <Tab label="LINKAGES" {...a11yProps(1)} />
@@ -1521,7 +1563,8 @@ export default function DataElementDetails() {
                                                     <TreeView
                                                         className={classes.derivatives}
                                                         defaultCollapseIcon={<ExpandMoreIcon />}
-                                                        expanded={expanded}
+                                                        defaultExpanded={expanded}
+                                                        // expanded={expanded}
                                                         defaultExpandIcon={<ChevronRightIcon />}
                                                         style={{ overflow: 'scroll' }}
                                                     >
@@ -1550,6 +1593,7 @@ export default function DataElementDetails() {
                                     </TabPanel>
                                     {pdhDerivatives = []}
                                     {derivedCoC = []}
+                                    </ErrorBoundary>
                                 </div>
                             </div>
                         </div>
