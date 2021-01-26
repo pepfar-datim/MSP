683cedb0cbd5ded7a243005399e32be135ae1a27
felixongati <fegati02@gmail.com>
2021-01-26 18:57:30 +0300

    update reference indicators query

--- src/src/AppShell/Indicators.js
+++ src/src/AppShell/Indicators.js
@@ -634,7 +634,7 @@ export default function Codelist() {
   const [search, setSearch] = React.useState(""); // set the search query string which is triggered by the search key
   const [searchInputText, setSearchInputText] = useState(""); // set the search text which is triggered on text change
   const [compareInputText, setCompareInputText] = useState(""); // set the search DE to compare
-  const queryIndicators = 'https://api.' + domain + '/orgs/' + org + '/sources/MER' + version + '/concepts/?verbose=true&q=&conceptClass=Reference Indicator&limit=0';
+  const queryIndicators = 'https://api.' + domain + '/orgs/' + org + '/sources/MER' + version + '/concepts/?verbose=true&q=&concept_class=Reference+Indicator&limit=999';
   const [indicators, setIndicators] = useState([""]);
   const [indicatorsTemp, setIndicatorsTemp] = useState([""]);
   const [indicatorQuery, setIndicatorQuery] = useState("");
@@ -1346,7 +1346,7 @@ useEffect(() => {
       }
       )
       UIDs = UIDs.substring(0, UIDs.length - 2)
-      downloadURL = 'https://api.' + domain + '/orgs/' + org + '/sources/MER/concepts/?paging=false&verbose=true&q=&limit=0&q=' + UIDs;
+      downloadURL = 'https://api.' + domain + '/orgs/' + org + '/sources/MER/concepts/?paging=false&verbose=true&q=&limit=999&q=' + UIDs;
     }
     else {
       downloadURL = 'https://datim.org/api/indicators' + '.' + downloadValue.trim() + '?filter=id:in:[' + selectedDataElement.toString().trim() + ']&fields=*&paging=false'
